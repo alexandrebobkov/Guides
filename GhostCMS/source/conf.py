7013b7361da2e310eeef554ea4f4acc274fefde0
Alexander B <alexandre.bobkov@gmail.com>
2025-06-29 04:46:01 -0400

    ghost

--- GhostCMS/source/conf.py
+++ GhostCMS/source/conf.py
@@ -14,7 +14,7 @@ release = '06-2025'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [simplepdf]
 
 templates_path = ['_templates']
 exclude_patterns = []
